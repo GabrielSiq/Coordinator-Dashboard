6735c71dc4d409e012e8ce99c15e43d3d9351801
Gabriel Siqueira <gabrielsiq@msn.com>
2017-03-14 22:30:59 -0300

    Added new dashboard style, started templating

--- application.py
+++ application.py
@@ -5,7 +5,17 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    return render_template('index.html')
+    
+
+    return render_template('dashboard.html')
+
+@application.route('/table')
+def table():
+    return render_template('table.html')
+
+@application.route('/template')
+def template():
+    return render_template('template.html')
 
 # @application.errorhandler(404)
 # def quatrozeroquatro(e):
