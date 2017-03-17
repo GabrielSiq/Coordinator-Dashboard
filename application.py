8ec363dfda1b8a61c145018fe89c0462cd22249d
gabrielsiq <gabrielsiq@msn.com>
2017-03-17 16:45:45 -0300

    Added raw data table and a few dashboard items

--- application.py
+++ application.py
@@ -1,16 +1,17 @@
 #coding: utf-8
 
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from urllib2 import urlopen
 import csv
 import os
 from apscheduler.schedulers.background import BackgroundScheduler
 import pandas as pd
-import numpy as np
 
 application = Flask(__name__)
 
 SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+
+DATA_SOURCE = pd.DataFrame()
 
 @application.before_first_request
 def initialize():
@@ -18,6 +19,7 @@ def initialize():
     Initializes our Flask app. Downloads student data and sets up a scheduler to re-download every day.
     """
     updateData()
+    loadData()
     scheduler = BackgroundScheduler()
     scheduler.start()
     scheduler.add_job(updateData, trigger = "interval", days = 1)
@@ -35,16 +37,27 @@ def updateData():
     for row in data:
         writer.writerow(row)
 
+def loadData():
+    csv_url = os.path.join(SITE_ROOT, 'static', 'assets', 'data', 'data.csv')
+    global DATA_SOURCE
+    DATA_SOURCE = pd.read_csv(csv_url, encoding="utf-8")
+    DATA_SOURCE.columns = ['matricula', 'periodo', 'disciplina', 'creditos', 'turma', 'grau', 'situacao', 'professor']
+
 @application.route('/dashboard')
 @application.route('/')
 def dashboard():
-    csv_url = os.path.join(SITE_ROOT, 'static', 'assets', 'data', 'data.csv')
-    df = pd.read_csv(csv_url, encoding = "utf-8")
-    return render_template('dashboard.html', df = df.head(10).to_dict())
+    global DATA_SOURCE
+
+    cancelation = DATA_SOURCE[DATA_SOURCE['situacao'].isin(['CA', 'CD', 'CL', 'DT', 'LT'])]
+    course_count = DATA_SOURCE.groupby('disciplina').size()
+    cancelation = cancelation.groupby('disciplina').size()
+    canc_rate = (cancelation / course_count).dropna()
+    return render_template('dashboard.html', df = DATA_SOURCE.head(10).to_dict(), canc = canc_rate.sort_values().head(10), canc2 = canc_rate.sort_values(ascending=False).head(10))
 
 @application.route('/table')
 def table():
-    return render_template('table.html')
+    global DATA_SOURCE
+    return render_template('table.html', df = DATA_SOURCE.head(50))
 
 @application.errorhandler(404)
 def not_found(error):
@@ -55,4 +68,5 @@ def server_error(error):
     return render_template('503.html')
 
 if __name__ == '__main__':
+    application.secret_key = 'some secret key'
     application.run()
