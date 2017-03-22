238380e5cdc4778d0c59015d777c2d4d790ce30d
gabrielsiq <gabrielsiq@msn.com>
2017-03-21 22:09:48 -0300

    First login attempt

--- application.py
+++ application.py
@@ -1,6 +1,10 @@
 #coding: utf-8
 
-from flask import Flask, render_template, flash, redirect, url_for
+from flask import Flask, render_template, flash, redirect, url_for, request, abort, Response
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+from wtforms import StringField, PasswordField, validators
+from flask_wtf import FlaskForm
+from urlparse import urlparse, urljoin
 from urllib2 import urlopen
 import csv
 import os
@@ -12,6 +16,70 @@ application = Flask(__name__)
 SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 
 DATA_SOURCE = pd.DataFrame()
+
+login_manager = LoginManager()
+login_manager.init_app(application)
+login_manager.login_view = "login"
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    password = PasswordField('Password', [validators.DataRequired()])
+
+
+# silly user model
+class User(UserMixin):
+    def __init__(self, id):
+        self.id = id
+        self.name = "user" + str(id)
+        self.password = self.name + "_secret"
+
+
+# create some users with ids 1 to 20
+users = [User(id) for id in range(1, 21)]
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
+
+def is_safe_url(target):
+    ref_url = urlparse(request.host_url)
+    test_url = urlparse(urljoin(request.host_url, target))
+    return test_url.scheme in ('http', 'https') and \
+           ref_url.netloc == test_url.netloc
+
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(u"Error in the %s field - %s" % (
+                getattr(form, field).label.text,
+                error
+            ))
+
+@application.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User(form.username._value())
+
+        login_user(user)
+
+        flash('Logged in successfully.')
+
+        next = request.args.get('next')
+        if not is_safe_url(next):
+            return abort(400)
+
+        return redirect(next or url_for('index'))
+    return render_template('login.html', form=form)
+
+@application.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("Logged out")
+    return redirect(request.referrer)
+
 
 @application.before_first_request
 def initialize():
@@ -38,6 +106,10 @@ def updateData():
         writer.writerow(row)
 
 def loadData():
+    """
+        Loads data onto global variable
+        TODO: this needs to change
+    """
     csv_url = os.path.join(SITE_ROOT, 'static', 'assets', 'data', 'data.csv')
     global DATA_SOURCE
     DATA_SOURCE = pd.read_csv(csv_url, encoding="utf-8")
@@ -48,6 +120,7 @@ def index():
     return redirect(url_for('dashboard'))
 
 @application.route('/dashboard')
+@login_required
 def dashboard():
     global DATA_SOURCE
 
